8616af774cf02101980a2208d313814fb34ace02
Mohammad <fathimohamad7379@gmail.com>
2025-11-01 17:16:47 +0330

    PUSH

--- CourseHub/Program.cs
+++ CourseHub/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<CourseHubContext>(options =>
-options.UseSqlServer("DefaultConnection"));
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
