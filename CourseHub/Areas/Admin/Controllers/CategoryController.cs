25d5f99dd24bdb6ee692d7aa055827dea778c92a
Mohammad <fathimohamad7379@gmail.com>
2025-11-05 16:21:50 +0330

    push

---
+++ CourseHub/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using CourseHub.Areas.Admin.Models.ViewModels;
+using CourseHub.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseHub.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private CourseHubContext _context;
+        public CategoryController(CourseHubContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var CategoryList = await _context.Categories
+                .Select(c => new GetAllCategoriesViewModel
+                {
+                    ID = c.CategoryID,
+                    Name = c.CategoryName,
+                    Slug = c.CategorySlug,
+                    ParentID = c.CategoryParentID,
+                    CoursesCount = c.Course_Categories.Count()
+                }).ToListAsync();
+            return View();
+        }
+    }
+}
---
+++ CourseHub/Areas/Admin/Models/ViewModels/GetAllCategoriesViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CourseHub.Areas.Admin.Models.ViewModels
+{
+    public class GetAllCategoriesViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public int? ParentID { get; set; }
+        public int CoursesCount { get; set; }
+    }
+}
