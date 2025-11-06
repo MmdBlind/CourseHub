f66018a5f685a546a927a2a1ae73dc0ef9af05fa
Mohammad <fathimohamad7379@gmail.com>
2025-11-06 16:03:16 +0330

    push

--- CourseHub/Areas/Admin/Controllers/CategoryController.cs
+++ CourseHub/Areas/Admin/Controllers/CategoryController.cs
@@ -1,10 +1,14 @@
 ﻿using CourseHub.Areas.Admin.Models.ViewModels;
+using CourseHub.Models.CourseDB;
+using CourseHub.Models.CourseHubDB;
 using CourseHub.Persistence;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace CourseHub.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private CourseHubContext _context;
@@ -26,7 +30,62 @@ namespace CourseHub.Areas.Admin.Controllers
                                     .FirstOrDefault(),
                     CoursesCount = c.Course_Categories.Count()
                 }).ToListAsync();
-            return View();
+
+            return View(CategoryList);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var parentOptions = _context.Categories
+                                    .AsNoTracking()
+                                    .OrderBy(c => c.CategoryName)
+                                    .Select(c => new SelectListItem
+                                    {
+                                        Value = c.CategoryID.ToString(),   // Value باید string باشد
+                                        Text = c.CategoryName
+                                    })
+                                    .ToList();
+
+            parentOptions.Insert(0, new SelectListItem { Value = null, Text = "بدون والد" });
+
+            var viewModel = new CreateAndEditCategoryViewModel
+            {
+                ParentOption = parentOptions   // حواست باشه اسم پراپرتی با ویو یکی باشه
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateAndEditCategoryViewModel viewModel)
+        {
+            var transaction=_context.Database.BeginTransaction();
+            if (_context.Categories.Where(c => c.CategorySlug == viewModel.Slug.Trim('-')).Any())
+            {
+                if (viewModel.Name != null && viewModel.Slug != null)
+                {
+                    Category category = new Category
+                    {
+                        CategoryName = viewModel.Name,
+                        CategorySlug = viewModel.Slug,
+                        CategoryParentID = viewModel.ParentID
+                    };
+                    await _context.Categories.AddAsync(category);
+                    await _context.SaveChangesAsync();
+                    transaction.Commit();
+                    return RedirectToAction("index");
+                }
+                else
+                {
+                    return RedirectToAction("create");
+                }
+            }
+            else
+            {
+                return RedirectToAction("create");
+            }
+        }
+
     }
 }
---
+++ CourseHub/Areas/Admin/Models/ViewModels/CreateAndEditCategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CourseHub.Areas.Admin.Models.ViewModels
+{
+    public class CreateAndEditCategoryViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public int ParentID { get; set; }
+        public IEnumerable<SelectListItem> ParentOption { get; set; }
+    }
+}
