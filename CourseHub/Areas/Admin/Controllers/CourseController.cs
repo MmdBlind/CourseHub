096119ef970a3916c2f1ca8c8f7e6851958b4c44
Mohammad <fathimohamad7379@gmail.com>
2025-11-13 16:50:40 +0330

    push

---
+++ CourseHub/Areas/Admin/Controllers/CourseController.cs
@@ -0,0 +1,22 @@
+﻿using CourseHub.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseHub.Areas.Admin.Controllers
+{
+    public class CourseController : Controller
+    {
+        private CourseHubContext _context;
+
+        public CourseController(CourseHubContext context)
+        {
+            _context = context;
+        }
+
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            var CourseList = _context.Courses
+            return View();
+        }
+    }
+}
